445e9eccab94a8e07220529bc904fe122e7a140a
asya afanasyeva <afanasyeva656@gmail.com>
2021-09-27 15:12:27 +0200

    Fixed some bugs in MyArrayList

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,27 +1,29 @@
-public class MyArrayList<T> implements IList<T>{
+public class MyArrayList<T> implements IList{
+
     private Object[] array = new Object[10];
     private int size = 0;
 
+    // Добавляет элемент в ArrayList. Если размер равен числу слотов array, то число слотов в array увеличивается
     @Override
-    public void add(T t) {
-        array[size] = t;
+    public void add(Object o) {
+        array[size] = o;
         size++;
 
-        if (size >= array.length) {
-            grow();
-        }
-
+        if (size == array.length) { grow(); }
     }
 
+    // Добавляет элемент на заданный индекса ArrayList, если такой существует.
+    // Если размер равен числу слотов array, то число слотов в array увеличивается
     @Override
-    public void add(int index, T t) {
-        if (index >= 0 && index < size) {
+    public void add(int index, Object o) {
+        if (index == size) { add(o); }
+        else if (index >= 0 && index < size) {
             Object[] tempArray = new Object[size + 1];
             int i = 0;
 
             for (int j = 0; j < tempArray.length; j++) {
                 if (j == index) {
-                    tempArray[j] = t;
+                    tempArray[j] = o;
                 } else {
                     tempArray[j] = array[i];
                     i++;
@@ -33,9 +35,10 @@ public class MyArrayList<T> implements IList<T>{
 
             if (size >= array.length) { grow(); }
         }
+        else { throw new IndexOutOfBoundsException(); }
     }
 
-
+    // Удаляет элемент на заданной позиции, если такой индекс существует.
     @Override
     public void remove(int index) {
         if (index >= 0 && index < size) {
@@ -43,24 +46,28 @@ public class MyArrayList<T> implements IList<T>{
                 array[i] = array[i + 1];
             }
             size--;
-        }
+        } else { throw new IndexOutOfBoundsException(); }
     }
 
+    // Удаляет элемент, если такой элемент существует.
+    // При отсутствии элемента ошибку не выбрасывает
     @Override
-    public void remove(T t) {
+    public void remove(Object o) {
         for (int i = 0; i < array.length; i++) {
-            if (t.equals(array[i])) {
+            if (o.equals(array[i])) {
                 remove(i);
+                size--;
                 break;
             }
         }
     }
 
+    // Заменяет элемент на указанной позиции на новый, если элемент с таким индексом существует.
     @Override
-    public void set(int index, T t) {
+    public void set(int index, Object o) {
         if (index >= 0 && index < size) {
-            array[index] = t;
-        }
+            array[index] = o;
+        } else { throw new IndexOutOfBoundsException(); }
     }
 
     @Override
@@ -69,12 +76,10 @@ public class MyArrayList<T> implements IList<T>{
     }
 
     @Override
-    public T get(int index) {
+    public Object get(int index) {
         if (index >= 0 && index < size) {
-            return (T) array[index];
-        } else {
-            return null;
-        }
+            return array[index];
+        } else { throw new IndexOutOfBoundsException(); }
     }
 
     private void grow() {
