43b006cfe98ba8d4d59d8995cd958937edcbebc4
asya afanasyeva <afanasyeva656@gmail.com>
2021-09-27 15:12:40 +0200

    Completed Task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,104 @@
+import org.w3c.dom.Node;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-// Операции удаления, добавления, поиска внутри каждой из структур
-// Написать собственную реализацию каждого из них через интерфейсы
 
 public class Main {
     public static void main(String[] args) {
-//        LinkedList<Integer> linkedList = new LinkedList<Integer>();
-//
-//        Integer a = 1;
-//        Integer b = 2;
-//
-//        linkedList.add(a);
-//        linkedList.add(b);
-//
-//        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-//        arrayList.add(a);
-//        arrayList.add(b);
-//        arrayList.add(0, 35);
-//
-//        for (int i = 0; i < linkedList.size(); i++) {
-//            System.out.println(linkedList.get(i));
-//        }
-//
-//        for (int i = 0; i < arrayList.size(); i++) {
-//            System.out.println(arrayList.get(i));
-//        }
+        System.out.println("<-- Реализация LinkedList -->");
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
+        Integer a = 1;
+        Integer b = 2;
 
-//        Реализация myArrayList
-        MyArrayList<Integer> myArrayList = new MyArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            myArrayList.add(i);
+        linkedList.add(a);
+        linkedList.add(b);
+        linkedList.add(3);
+        linkedList.add(0,21);
+
+        linkedList.remove(1);
+        linkedList.set(0, 33);
+
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println("На позиции " + i + " - " + linkedList.get(i));
         }
 
-        myArrayList.add(2,35);
-        myArrayList.remove(12);
-        myArrayList.add(14, 145);
+        System.out.println("");
 
-        System.out.println("Size of array: " + myArrayList.size());
+        System.out.println("<-- Реализация ArrayList -->");
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(a);
+        arrayList.add(b);
+        arrayList.add(2, 35);
+        arrayList.add(4);
+
+        arrayList.remove(0);
+        arrayList.set(0, 22);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println("На позиции " + i + " - " + arrayList.get(i));
+        }
+
+        System.out.println("");
+
+        System.out.println("<-- Реализация myArrayList -->");
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        myArrayList.add("Маркетинг");
+        myArrayList.add("Продажи");
+        myArrayList.add("Поддержка");
+        myArrayList.add("Бухгалтерия");
+        myArrayList.add(4,"Производство");
+
+
         for (int i = 0; i < myArrayList.size(); i++) {
-            System.out.println("Value with index " + i + " is " + myArrayList.get(i));
+            System.out.println("На позиции " + i + " - " + myArrayList.get(i));
+        }
+
+        myArrayList.remove("Продажи");
+        myArrayList.set(0, "Продажи и Маркетинг");
+        System.out.println("Удалили Продажи и заменили отдел Маркетинг на Продажи и Маркетинг (по индексу 0)");
+        for (int i = 0; i < myArrayList.size(); i++) {
+            System.out.println("На позиции " + i + " - " + myArrayList.get(i));
+        }
+
+        try {
+            myArrayList.set(21, "Отдел снабжения");
+        } catch (IndexOutOfBoundsException error) {
+            System.out.println("Выброшена ошибка IndexOutOfBoundsException в myArrayList");
+        }
+
+        System.out.println("");
+
+        System.out.println("<-- Реализация myLinkedList -->");
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add("Оля");
+        myLinkedList.add("Женя");
+        myLinkedList.add(1, "Вадим");
+        myLinkedList.add("Алина");
+        myLinkedList.add("Слава");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println("На позиции " + i + " - " + myLinkedList.get(i));
+        }
+
+        myLinkedList.remove(0);
+        myLinkedList.remove("Женя");
+        System.out.println("Удалили Олю (по индексу 0) и Женю");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println("На позиции " + i + " - " + myLinkedList.get(i));
+        }
+
+        myLinkedList.add("Катя");
+        myLinkedList.set(2, "Никита");
+        System.out.println("Добавили Катю и заменили Славу (по индексу 2) на Никиту");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println("На позиции " + i + " - " + myLinkedList.get(i));
+        }
+
+        try {
+            myLinkedList.set(21, "Артем");
+        } catch (IndexOutOfBoundsException error) {
+            System.out.println("Выброшена ошибка IndexOutOfBoundsException в myLinkedList");
         }
     }
 }
